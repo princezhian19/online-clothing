728f2d35bdf9de8a3b1dcd538e71a9b967d5ed3b
noeloariola <noeloariola@gmail.com>
2022-12-03 18:38:22 +0800

    fixed for admin orders view sidebar

--- view-orderAdmin.php
+++ view-orderAdmin.php
@@ -97,32 +97,8 @@ if ($_SESSION['userrole'] == 1) {
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fa-brands fa-dropbox"></i>
-                    <span>Products</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Products:</h6>
-                        <a class="collapse-item" href="add-product.php">Add Products</a>
-                        <a class="collapse-item" href="view-products.php">View Products</a>
-                    </div>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Orders</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Orders</h6>
-                        <a class="collapse-item" href="orders.php">View orders</a>
-
-                    </div>
-                </div>
-            </li>
+            
+            <?php include 'template/navitem.php'; ?>
 
 
 
